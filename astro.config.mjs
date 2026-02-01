d500381431a9887dff355a18676980333773e6df
A-PachecoT <andre.pacheco.t@uni.pe>
2026-02-01 10:52:29 -0500

    fix: remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wquiceno.lat",
-  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
